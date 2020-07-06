b838b9aefce0e10191d0fcf5618f06e0caff41dd
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-07-06 23:12:38 +0300

    using tinyMCE  for the content of the article

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -188,85 +188,8 @@
             @endif
         </div>
     </div>
-    {{--
-        <div class="container">
-            <div class="row justify-content-center">
-                <div><a class="nav-link navbar-icon text-secondary" href="#"><i class="fa fa-sort-asc fa-2x fa-rotate-270"
-                                                                                aria-hidden="true"></i></a>
-                </div>
-                <div class="scrollmenu col-md-8">
-                    <a href="#home">Home</a>
-                    <a href="#news">News</a>
-                    <a href="#contact">Contact</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                    <a href="#about">About</a>
-                </div>
-                <div><a class="nav-link navbar-icon text-secondary" href="#"><i class="fa fa-sort-asc fa-2x fa-rotate-90"
-                                                                                aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            <div class="container text-center my-3">
-                <div class="row mx-auto my-auto">
-                    <div id="recipeCarousel" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner " role="listbox">
-                            @for ($i = 1; $i < 10; $i++)
-                                <div class="carousel-item active">
-                                    <div class="col-lg-2 col-md-6">
-                                        <div class="movie-card m-1">
-                                            <div class="movie-img">
-                                                <a href="#"><img
-                                                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT9i-t0o_ltZf_c5ZQ4F4kbuETdhNDdxsjIYKaofkjTM3BmHTqc"
-                                                        class="img-fluid"></a>
-                                            </div>
-                                            <div class="movie-title">
-                                                <a href="#"><p class="text-white-50 text-sm-center font-small flex-center">IRON MAN</p></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endfor
-                            <a class="carousel-control-prev bg-dark w-auto" href="#recipeCarousel" role="button"
-                               data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next bg-dark w-auto" href="#recipeCarousel" role="button"
-                               data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header">{{ __('Dashboard') }}</div>
-
-                            <div class="card-body">
-                                @if (session('status'))
-                                    <div class="alert alert-success" role="alert">
-                                        {{ session('status') }}
-                                    </div>
-                                @endif
-
-                                {{ __('You are logged in!') }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>--}}
 @endsection
+
+@push('script')
+
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,89 +12,100 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            menubar: false
 
+        });
+    </script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
 
 
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset('/svg/medium-vector-logo.svg')}}"
-                              alt="HealthHub logo"
-                              class="pr-3">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{asset('/svg/medium-vector-logo.svg')}}"
+                     alt="HealthHub logo"
+                     class="pr-3">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarText">
-
-
-                </div>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                        </li>
-                    </ul>
-
-                    <a class="nav-link navbar-icon" href="#"><i class="fa fa-search fa-lg" aria-hidden="true"></i></a>
-                    <a class="nav-link navbar-icon" href="#"><i class="fa fa-bookmark fa-lg" aria-hidden="true"></i></a>
-                    <a class="nav-link navbar-icon" href="#"><i class="fa fa-bell-o fa-lg" aria-hidden="true"></i></a>
-                    <button type="button" class="btn btn-outline-secondary font-weight-bold">Upgrade</button>
+            <div class="collapse navbar-collapse" id="navbarText">
 
 
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-3">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <div class="dropdown navbar-user-dropdown">
-                                <button class="btn btn-secondary dropdown-toggle btn-circle" type="button"
-                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false"></button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="#">Become a member</a>
-                                    <a class="dropdown-item" href="{{route('article.create')}}">New story</a>
-                                    <a class="dropdown-item" href="#">Stories</a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        sign out
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </div>
-                        @endguest
-                    </ul>
-                </div>
             </div>
-        </nav>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                    </li>
+                </ul>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+                <a class="nav-link navbar-icon" href="#"><i class="fa fa-search fa-lg" aria-hidden="true"></i></a>
+                <a class="nav-link navbar-icon" href="#"><i class="fa fa-bookmark fa-lg" aria-hidden="true"></i></a>
+                <a class="nav-link navbar-icon" href="#"><i class="fa fa-bell-o fa-lg" aria-hidden="true"></i></a>
+                <button type="button" class="btn btn-outline-secondary font-weight-bold">Upgrade</button>
+
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-3">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <div class="dropdown navbar-user-dropdown">
+                            <button class="btn btn-secondary dropdown-toggle btn-circle" type="button"
+                                    id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false"></button>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Become a member</a>
+                                <a class="dropdown-item" href="{{route('article.create')}}">New story</a>
+                                <a class="dropdown-item" href="#">Stories</a>
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    sign out
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </div>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+@stack('script')
 </body>
 </html>

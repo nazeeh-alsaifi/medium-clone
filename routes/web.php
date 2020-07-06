27053cae5bf13b7d795e1f72990aa43bf163b7f3
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-07-06 22:19:46 +0300

    setting up a route to create articles and creating the method in the controller

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//-----------articles routes -----------
+Route::get('article/create', 'ArticleController@create')->name('article.create');

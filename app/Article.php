f0039a1aee304613df23f88952b7ba40d76d461e
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-07-07 19:28:10 +0300

    set up the relationship between article and tag

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'description', 'content'];
+
+    public function tags(){
+        $this->hasMany('App\Tag');
+    }
 }
